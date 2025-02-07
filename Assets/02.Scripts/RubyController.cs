5ffcd2d9d6bfc6e9b43d7010d206bc672a04ea82
hgkang2 <hgkang2hank@naver.com>
2025-02-07 15:43:10 +0900

    루비체력

--- Assets/02.Scripts/RubyController.cs
+++ Assets/02.Scripts/RubyController.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public float moveSpeed = 5.0f;
+    public int maxHealth = 5;
+    public int health {get { return currentHealth;}}
+    int currentHealth;
     Rigidbody2D rb2D;
     // Start is called before the first frame update
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+        currentHealth = 1;
     }
 
     // Update is called once per frame
@@ -17,8 +23,14 @@ public class RubyController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
        float vertical = Input.GetAxis("Vertical");
        Vector2 position = rb2D.position;
-       position.x = position.x + 20.0f * horizontal * Time.deltaTime;
-       position.y = position.y + 20.0f * vertical * Time.deltaTime;
+       position.x = position.x + moveSpeed * horizontal * Time.deltaTime;
+       position.y = position.y + moveSpeed * vertical * Time.deltaTime;
        rb2D.MovePosition(position);
+    }
+   public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        //Debug.Log(currentHealth + "/" + maxHealth);
+        Debug.Log($"{currentHealth}/{maxHealth}");
     }
 }
